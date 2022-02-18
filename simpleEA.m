1ae42cdfdac07181ec672190020a03d91207d386
Uchimaki Tamako <11912921@mail.sustech.edu.cn>
2022-02-18 20:06:36 +0800

    Complete simpleEA

--- simpleEA.m
+++ simpleEA.m
@@ -29,23 +29,89 @@ fitness_pop=[];% record the best fitness in current population
 %% Below starting your code
 
 % Initialise a population
-%% TODO
-
+populationSize = 4;
+lowerBound = 0;
+upperBound = 31;
+lenGene = 5;
+population = randi([lowerBound,upperBound],populationSize,1);
+genotypes = dec2bin(population);
 
 % Evaluate the initial population
-%% TODO
+fitness = objFunc(population);
+[A,index] = sort(fitness,1,'descend');
+fitness_pop = [fitness_pop,A(1)];
+
+for i=1:populationSize
+    if fitness(i)>bestSoFarFit
+        bestSoFarFit = fitness(i);
+        bestSoFarSolution = population(i);
+        cnt = 0;
+    end
+end
+
+nbEval = nbEval + populationSize;
+nbGen =  nbGen + 1;
+fitness_gen = horzcat(fitness_gen, bestSoFarFit);
+solution_gen = horzcat(solution_gen, bestSoFarSolution);
 
 % Start the loop
-while (nbEval<T) 
+while (nbEval<T && cnt<20) 
 % Reproduction (selection, crossver)
-%% TODO
+    crossoverProb = fitness./sum(fitness);
+    offspringGenes = [];
+    for i = 1:populationSize/2
+    parentIndexes = [];
+    for j = 1:2
+        r = rand();
+            for index = 1:populationSize
+                if r>sum(crossoverProb(1:index-1)) && r<=sum(crossoverProb(1:index))
+                    break;
+                end
+            end
+            parentIndexes = [parentIndexes, index];
+        end
+        crossoverPoint = randi(lenGene-1);
+        offspringGenes = [offspringGenes; [genotypes(parentIndexes(1),1:crossoverPoint), genotypes(parentIndexes(2),crossoverPoint+1:end)]];
+        offspringGenes = [offspringGenes; [genotypes(parentIndexes(2),1:crossoverPoint), genotypes(parentIndexes(1),crossoverPoint+1:end)]];
+    end
 
-% Mutation
-%% TODO
+    % Mutation
+    mutationProb = 1/lenGene;
+    for i = 1:populationSize
+        isMutation = rand(1,lenGene) < mutationProb;
+        offspringGenes(i,isMutation) = dec2bin('1'- offspringGenes(i,isMutation))';
+    end
+
+    genotypes = offspringGenes;
+    population = bin2dec(genotypes);
+    fitness = objective(population);
+    [A,index] = sort(fitness,1,'descend');
+    fitness_pop=[fitness_pop,A(1)];
+    for i = 1:populationSize
+        if fitness(i) > bestSoFarFit
+            bestSoFarFit = fitness(i);
+            bestSoFarSolution = population(i,:);
+        end
+    end
+    if bestSoFarFit == fitness_gen(end)
+        cnt = cnt + 1;
+    else
+        cnt = 0;
+    end
+
+    nbEval = nbEval + populationSize;
+    nbGen = nbGen + 1;
+    fitness_gen=horzcat(fitness_gen, bestSoFarFit);
+    solution_gen=horzcat(solution_gen, bestSoFarSolution);
+end
+
 
 
 bestSoFarFit
 bestSoFarSolution
+fitness_pop
+fitness_gen
+solution_gen
 
 
 
